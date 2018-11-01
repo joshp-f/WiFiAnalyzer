7c80b72c0e7712c64922cef203075ab168aa8b5e
dyspr0sium <dy.dx@hotmail.com>
2018-11-01 23:11:22 +1100

    Updated app to take a distance as input.

--- WiFiAPScanner/app/src/main/java/comp6733/wifiapscanner/MainActivity.kt
+++ WiFiAPScanner/app/src/main/java/comp6733/wifiapscanner/MainActivity.kt
@@ -7,11 +7,11 @@ import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import android.widget.ToggleButton
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.WorkManager
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.*
 import java.util.concurrent.TimeUnit
+import android.widget.EditText
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,22 +21,37 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
-            Toast.makeText(applicationContext, "Writing WiFi scan to file", Toast.LENGTH_LONG).show()
-            val wifiScanWork = OneTimeWorkRequestBuilder<WiFiScannerWorker>().build()
+            val simpleEditText = findViewById<EditText>(R.id.distanceText)
+            val str = simpleEditText.text.toString()
+            Toast.makeText(applicationContext, "Starting Single 3x Scan...", Toast.LENGTH_LONG).show()
+            val wifiScanWork: OneTimeWorkRequest
+            if (str == "") {
+                wifiScanWork = OneTimeWorkRequestBuilder<WiFiScannerWorker>().setInputData(Data.Builder().putInt("mode", 1).build()).build()
+            } else {
+                wifiScanWork = OneTimeWorkRequestBuilder<WiFiScannerWorker>().setInputData(Data.Builder().putInt("mode", 1).putFloat("dist", str.toFloat()).build()).build()
+            }
             WorkManager.getInstance().enqueue(wifiScanWork)
         }
 
         val button2 = findViewById<Button>(R.id.button2)
         button2.setOnClickListener {
-            Toast.makeText(applicationContext, "Starting Periodic Scan", Toast.LENGTH_LONG).show()
-            val periodicWifiScanWork = PeriodicWorkRequestBuilder<WiFiScannerWorker>(15, TimeUnit.MINUTES).build()
+            Toast.makeText(applicationContext, "Starting Periodic 3x Scan...", Toast.LENGTH_LONG).show()
+            val periodicWifiScanWork = PeriodicWorkRequestBuilder<WiFiScannerWorker>(15, TimeUnit.MINUTES).setInputData(Data.Builder().putInt("mode", 1).build()).build()
             WorkManager.getInstance().enqueueUniquePeriodicWork("Periodic Scan", ExistingPeriodicWorkPolicy.KEEP, periodicWifiScanWork)
         }
 
         val button3 = findViewById<Button>(R.id.button3)
         button3.setOnClickListener {
-            Toast.makeText(applicationContext, "Stopping Periodic Scan", Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext, "Stopping Periodic 3x Scan...", Toast.LENGTH_LONG).show()
             WorkManager.getInstance().cancelUniqueWork("Periodic Scan")
         }
+
+        val button4 = findViewById<Button>(R.id.button4)
+        button4.setOnClickListener {
+            Toast.makeText(applicationContext, "Starting Single 3x Scan...", Toast.LENGTH_LONG).show()
+            val wifiScanSendWork = OneTimeWorkRequestBuilder<WiFiScannerWorker>().setInputData(Data.Builder().putInt("mode", 2).build()).build()
+            WorkManager.getInstance().enqueue(wifiScanSendWork)
+        }
+
     }
 }
--- WiFiAPScanner/app/src/main/java/comp6733/wifiapscanner/WiFiScannerWorker.kt
+++ WiFiAPScanner/app/src/main/java/comp6733/wifiapscanner/WiFiScannerWorker.kt
@@ -11,45 +11,80 @@ import android.util.Log
 import android.widget.Toast
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.android.volley.Request
+import com.android.volley.toolbox.Volley
 import java.io.File
 import java.io.FileOutputStream
 import java.sql.Timestamp
-import java.text.SimpleDateFormat
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import android.provider.Settings.Secure
+
 
 class WiFiScannerWorker(context : Context, params : WorkerParameters) : Worker(context, params) {
 
     override fun doWork(): Result {
-        var resultList: ArrayList<ScanResult>
-        lateinit var wm: WifiManager
+        var count = 1
+        val timestamp = Timestamp(System.currentTimeMillis()).toString()
+        val wm: WifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val mode = inputData.getInt("mode", 0)
+        val dist = inputData.getFloat("dist", (-1).toFloat())
+        var output = if (dist == (-1).toFloat()) "Time Stamp|SSID|BSSID|RSSI\n" else "Time Stamp|SSID|BSSID|RSSI|dist\n"
 
         val broadcastReceiver = object : BroadcastReceiver() {
             override fun onReceive(contxt: Context?, intent: Intent?) {
-                var output = "Time Stamp|SSID|BSSID|RSSI\n"
-                val timestamp = Timestamp(System.currentTimeMillis()).toString()
-                resultList = wm.scanResults as ArrayList<ScanResult>
+                val resultList = wm.scanResults as ArrayList<ScanResult>
+                Log.d("TESTING", "WiFi scan " + count.toString() + " received")
                 Log.d("TESTING", resultList.toString())
-                applicationContext.unregisterReceiver(this)
 
                 for (result in resultList) {
-                    output += timestamp + "|" + result.SSID + "|" + result.BSSID + "|" + result.level + "\n"
+                    if (dist == (-1).toFloat()) {
+                        output += timestamp + "|" + result.SSID + "|" + result.BSSID + "|" + result.level + "\n"
+                    } else {
+                        output += timestamp + "|" + result.SSID + "|" + result.BSSID + "|" + result.level + "|" + dist + "\n"
+                    }
                 }
 
-                val dir = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "WiFi Logs")
-                if (dir.mkdirs()) {
-                    Log.d("Debugging Message", "Directory created")
-                }
-                try {
-                    val file = File(dir.absolutePath + "/log.txt")
-                    val outputStream = FileOutputStream(file, true)
-                    outputStream.write(output.toByteArray())
-                    outputStream.close()
-                } catch (e: Exception) {
-                    e.printStackTrace()
+                count++
+                if (count <= 3) {
+                    wm.startScan()
+                } else {
+                    applicationContext.unregisterReceiver(this)
+                    // Write to file
+                    if (mode == 1) {
+                        Toast.makeText(applicationContext, "Scan complete. Writing WiFi scan to file.", Toast.LENGTH_LONG).show()
+                        val dir = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "WiFi Logs")
+                        if (dir.mkdirs()) {
+                            Log.d("Debugging Message", "Directory created")
+                        }
+                        try {
+                            val file = File(dir.absolutePath + "/log.txt")
+                            val outputStream = FileOutputStream(file, true)
+                            outputStream.write(output.toByteArray())
+                            outputStream.close()
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                        // Send to server
+                    } else if (mode == 2) {
+                        Toast.makeText(applicationContext, "Scan complete. Sending WiFi scan to server.", Toast.LENGTH_LONG).show()
+                        //Log.d("output", output)
+                        val url = "https://iotproximity.herokuapp.com"
+                        val pr = object : StringRequest(Request.Method.POST, url, Response.Listener { response -> Log.d("POST Response", response);  Toast.makeText(applicationContext, "Jaccard Coefficient = $response", Toast.LENGTH_LONG).show() }, Response.ErrorListener { Log.d("POST Error", "") }) {
+                            override fun getParams(): Map<String, String> {
+                                val params = HashMap<String, String>()
+                                params["data"] = output
+                                params["id"] = Secure.getString(applicationContext.contentResolver, Secure.ANDROID_ID)
+                                Log.d("ID", Secure.getString(applicationContext.contentResolver, Secure.ANDROID_ID))
+                                return params
+                            }
+                        }
+                        //val pr = StringRequest(Request.Method.GET, url, Response.Listener<String> { response -> Log.d("GET response", response) }, Response.ErrorListener {Log.d("GET error", "GET error")})
+                        Volley.newRequestQueue(applicationContext).add(pr)
+                    }
                 }
             }
         }
-
-        wm = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         wm.startScan()
         applicationContext.registerReceiver(broadcastReceiver, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
         return Result.SUCCESS
